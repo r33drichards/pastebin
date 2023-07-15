c0c49315af62ba35a1c7fefc07fc8883a24d7784
Robert <xxmrmau5@gmail.com>
2023-07-15 03:01:21 +0000

    agen

--- infrastructure.tf
+++ infrastructure.tf
@@ -78,49 +78,49 @@ resource "aws_apprunner_service" "pbin" {
 }
 
 
-# module "tailscale_instance" {
-#   source = "./modules/tailscale-instance"
-#   aws_region = var.aws_region
-#   aws_availability_zone = "us-east-1a"
-#   user_data = <<EOF
-# # install docker 
-# sudo apt-get update
-# sudo apt install docker.io -y
-# sudo systemctl start docker
-# sudo systemctl enable docker
+module "tailscale_instance" {
+  source = "./modules/tailscale-instance"
+  aws_region = var.aws_region
+  aws_availability_zone = "us-east-1a"
+  user_data = <<EOF
+# install docker 
+sudo apt-get update
+sudo apt install docker.io -y
+sudo systemctl start docker
+sudo systemctl enable docker
 
-# sudo apt  install awscli -y
+sudo apt  install awscli -y
 
-# # login to ecr
-# eval $(aws ecr get-login --no-include-email --region us-east-1)
+# login to ecr
+eval $(aws ecr get-login --no-include-email --region us-east-1)
 
-# # docker run --env-file .env -p 8000:8000 pbin:latest 
-# sudo docker run -d --restart=always \
-#   -e AWS_ACCESS_KEY_ID=${var.pbin_aws_access_key_id} \
-#   -e AWS_SECRET_ACCESS_KEY=${var.pbin_aws_secret_access_key} \
-#   -e AWS_REGION=${var.aws_region} \
-#   -e PBIN_TABLE_NAME=${var.pbin_table_name} \
-#   -e PBIN_URL=tsbin:8000 \
-#   -e OPENAPIKEY=${var.openapikey} \
-#   -p 8000:8000 ${aws_ecr_repository.pbin.repository_url}:${var.image_tag}
+# docker run --env-file .env -p 8000:8000 pbin:latest 
+sudo docker run -d --restart=always \
+  -e AWS_ACCESS_KEY_ID=${var.pbin_aws_access_key_id} \
+  -e AWS_SECRET_ACCESS_KEY=${var.pbin_aws_secret_access_key} \
+  -e AWS_REGION=${var.aws_region} \
+  -e PBIN_TABLE_NAME=${var.pbin_table_name} \
+  -e PBIN_URL=tsbin:8000 \
+  -e OPENAPIKEY=${var.openapikey} \
+  -p 8000:8000 ${aws_ecr_repository.pbin.repository_url}:${var.image_tag}
 
-# EOF
-# # admin access 
-#   iam_policy = <<EOF
-# {
-#     "Version": "2012-10-17",
-#     "Statement": [
-#         {
-#             "Sid": "VisualEditor1",
-#             "Effect": "Allow",
-#             "Action": "*",
-#             "Resource": "*"
-#         }
-#     ]
+EOF
+# admin access 
+  iam_policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor1",
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
+    ]
 
-# }
-# EOF
-#   name = "tsbin"
-#   tailscale_auth_key = var.tailscale_auth_key
+}
+EOF
+  name = "tsbin"
+  tailscale_auth_key = var.tailscale_auth_key
 
-# }
+}
