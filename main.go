02c3927e9b18954eebe3eeed0f19431661332ae3
Robert <xxmrmau5@gmail.com>
2023-07-15 05:53:07 +0000

    disable completions until i add users

--- main.go
+++ main.go
@@ -559,7 +559,7 @@ func main() {
 	logger, _ := zap.NewProduction()
 	defer logger.Sync() // flushes buffer, if any
 	sugar := logger.Sugar()
-	handleWithDefaultRateLimiter("/complete", handleCompletion(sugar))
+	// handleWithDefaultRateLimiter("/complete", handleCompletion(sugar))
 	handleWithDefaultRateLimiter("/diff", handleDiff)
 	handleWithDefaultRateLimiter("/health", handleHealth)
 	handleWithDefaultRateLimiter("/paste", handlePaste)
