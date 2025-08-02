4f46bbd273e871f634baec5435496772718362de
r33drichards <rwendt1337@gmail.com>
2025-08-01 21:21:02 -0700

    f

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
+import MainPage from './pages/MainPage'
 import HomePage from './pages/HomePage'
 import PastePage from './pages/PastePage'
 import DiffPage from './pages/DiffPage'
 import HtmlPage from './pages/HtmlPage'
-import BufferTestPage from './pages/BufferTestPage'
 import NotFoundPage from './pages/NotFoundPage'
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<HomePage />} />
+      <Route path="/" element={<MainPage />} />
+      <Route path="/home" element={<HomePage />} />
       <Route path="/paste" element={<PastePage />} />
       <Route path="/diff" element={<DiffPage />} />
       <Route path="/html" element={<HtmlPage />} />
-      <Route path="/buffers" element={<BufferTestPage />} />
-      <Route path="/buffer" element={<NotFoundPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
   )
--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import BufferManager from '../components/BufferManager'
 
-export default function BufferTestPage() {
+export default function MainPage() {
   return (
     <div className="h-screen w-screen bg-gray-50 dark:bg-gray-900">
       <BufferManager initialContent="// Type your code here" language="javascript" />
